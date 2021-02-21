1bd1ee60096a635f151dac9fd09626f4bbd79885
Arjane Lolong <rjn.sachi@gmail.com>
2021-02-21 21:17:27 +0800

    feat: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6788bced75057791947525ef2f56c5f8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c9d131e4-9238-48d3-96e1-7d30082a47c8'
+};
