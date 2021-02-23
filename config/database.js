f00c24885dc244d10c9847d26ab082e8b099455b
Arjane Lolong <rjn.sachi@gmail.com>
2021-02-23 16:35:36 +0800

    fix: use env.int for db port

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST'),
-        port: env('DATABASE_PORT'),
+        port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
